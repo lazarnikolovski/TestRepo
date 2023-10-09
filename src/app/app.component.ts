011a0acccc55460e91a15b72f61cd02235113c0c
lazarnikolovski <lazar.nikolovski@tylertech.com>
2023-10-09 17:18:13 -0400

    Learning angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBasics';
+  title = 'Angular Basics';
+  message = "Lazar is learning";
 }
